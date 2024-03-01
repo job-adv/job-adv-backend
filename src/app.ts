97cf443022037b1fdf7c4e7374521103c4099c08
abdelkader_kdr <kadouriabdelkader20@gmail.com>
2024-03-01 20:13:08 +0100

    second commit

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,7 @@ dotenv.config();
 const app: express.Application = express();
 const Port = process.env.PORT || 3000;
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({ limit: "20kb"}));
 app.use(express.urlencoded({ extended: true }));
 
 
